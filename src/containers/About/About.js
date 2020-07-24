86b781f59af7eac7abaf6985a02d7081be0447cf
maqiling <maqiling@shanda.com>
2020-07-24 17:35:25 +0800

    format

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import {
-  Switch,
-  Link
-} from "react-router-dom";
-import RouteWithSubRoutes from 'routes/withSubRoute'
+import React from "react";
+import { Switch, Link } from "react-router-dom";
+import RouteWithSubRoutes from "routes/withSubRoute";
 
-const About =({routes}) => {
+const About = ({ routes }) => {
   return (
     <div>
       About page
-
       <div>
-        <Link to='/about/me'>Me</Link> | 
-        <Link to='/about/company'> Company</Link>
+        <Link to="/about/me">Me</Link> |
+        <Link to="/about/company"> Company</Link>
       </div>
       <Switch>
-      {routes.map((route, i) => (
+        {routes.map((route, i) => (
           <RouteWithSubRoutes key={route.path} {...route} />
         ))}
       </Switch>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
